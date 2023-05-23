0275569f783219e9faa30dc3d96404617707f353
Taras Horb <petrovo2222@gmail.com>
2023-05-22 23:12:50 -0400

    cheng master

--- src/route/index.js
+++ src/route/index.js
@@ -359,6 +359,15 @@ router.get('/person', function (req, res) {
                       ],
                     },
                   ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  })
+})
 
 router.get('/bio', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
@@ -451,6 +460,10 @@ router.get('/bio', function (req, res) {
         'Leonardo designed and invented a wide variety of machines and devices, including a helicopter, a diving suit, and a self-propelled cart. Many of his inventions were centuries ahead of their time.',
       year: 'Late 15th to early 16th century',
       field: 'Invention',
+    },
+  })
+})
+
       
 router.get('/web', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
