03415739f7cc13024ffb321711b645dce162a1f8
Taras Horb <petrovo2222@gmail.com>
2023-05-26 06:43:47 -0400

    bootstrap

--- src/route/index.js
+++ src/route/index.js
@@ -748,6 +748,8 @@ router.get('/facebook', function (req, res) {
         ],
       },
     ],
+  })
+})
 
 
 router.get('/mac', function (req, res) {
@@ -833,6 +835,9 @@ router.get('/mac', function (req, res) {
           price: 19.99,
         },
       ],
+    },
+  })
+})
 
 router.get('/car', function (req, res) {
   // res.render генерує нам HTML сторінку
@@ -1014,6 +1019,22 @@ router.get('/js', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'default',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 
 // Підключаємо роутер до бек-енду
 
